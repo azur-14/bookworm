84c66bc0abafa890e86b6b772ab32022eada707b
Đoàn Thống Lĩnh <52200013@student.tdtu.edu.vn>
2025-04-28 17:06:53 +0700

    add rR

---
+++ bookworm/service/RequestService/routes/returnRequest.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const ReturnRequest = require('../models/ReturnRequest');
+const BorrowRequest = require('../models/BorrowRequest');
+
+// lấy kết quả mượn sách của người dùng hiện tại
+router.get('/user/:userId', async (req, res) => {
+    try {
+      const borrowIds = await BorrowRequest.find({ user_id: req.params.userId }).distinct('id');
+  
+      const requests = await ReturnRequest.find().where('borrow_request_id').in(borrowIds);
+      res.json(requests);
+    } catch (err) {
+      console.error('❌ Error in return-requests:', err);
+      res.status(500).json({ error: 'Failed to fetch return requests' });
+    }
+});  
+
+module.exports = router;
