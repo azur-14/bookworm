3038f00d863de1c05cec7a2b9f3e921f6bc6a5fd
Đoàn Thống Lĩnh <52200013@student.tdtu.edu.vn>
2025-05-03 19:24:54 +0700

    add attribute activity log

--- bookworm/service/AdminService/models/ActivityLog.js
+++ bookworm/service/AdminService/models/ActivityLog.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
 const activityLogSchema = new mongoose.Schema({
-  logs_id: { type: String, required: true, unique: true },
-  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  action: { type: String, required: true },
-  timestamp: { type: Date, default: Date.now }
+  adminId: { type: String, required: true }, //người thực hiện hđ
+  actionType: { type: String, enum: ['CREATE', 'UPDATE', 'DELETE', 'APPROVE', 'REJECT', 'LOGIN', 'LOGOUT'], required: true },
+  targetType: { type: String, required: true },
+  targetId: { type: String, required: true }, //người bị tác động
+  description: { type: String, default: '' }, //Mô tả chi tiết hành động (VD: "Updated user email from A to B").
+  timestamp: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('ActivityLog', activityLogSchema);
